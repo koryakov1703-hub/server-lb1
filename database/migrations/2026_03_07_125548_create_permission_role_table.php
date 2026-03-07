a4f44a6cbd876ad0112d5fc6f20a7c33132b06c0
Grisha <koryakov.1703@gmail.com>
2026-03-07 13:01:25 +0500

    Добавлены миграции ролей, разрешений и связующих таблиц

---
+++ database/migrations/2026_03_07_125548_create_permission_role_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('permission_id')
+                ->constrained('permissions')
+                ->cascadeOnDelete();
+
+            $table->foreignId('role_id')
+                ->constrained('roles')
+                ->cascadeOnDelete();
+
+            $table->foreignId('created_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            $table->timestamp('created_at')->nullable();
+
+            $table->softDeletes();
+
+            $table->foreignId('deleted_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('permission_role');
+    }
+};
